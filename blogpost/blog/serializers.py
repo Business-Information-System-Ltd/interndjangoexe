97c039cf82a39fcc19c4b6400f81aacab5914f1a
Wint Nandar Lynn <wnlynn85@gmail.com>
2026-05-08 09:30:57 +0630

    blogpostexe

---
+++ blogpost/blog/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import User,Category,Post
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields ='__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields='__all__'
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields='__all__'
