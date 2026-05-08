00f941baaa5a4dcf8a97cb2211dad1107d09088f
hninmyatwai521 <hninmyatwai521@gmail.com>
2026-05-08 09:27:27 +0630

    nn

--- blogpost/blog/models.py
+++ blogpost/blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+
+class User(models.Model):
+       user_id = models.AutoField(primary_key=True)
+       name = models.CharField(max_length=100)
+       email = models.CharField(max_length=100,null=False)
+       password = models.CharField(max_length=100,null=False)
+       role = models.CharField(max_length=100,null=False)
+       class Meta:
+           db_table = 'user'
+
+       def __str__(self):
+           return self.name
+
+#.......category.......
+class Category(models.Model):
+       category_id = models.AutoField(primary_key=True)
+       name = models.CharField(max_length=100)
+       description = models.CharField(max_length=255)
+       created_at = models.DateTimeField(auto_now_add=True)
+
+       class Meta:
+         db_table = 'category'
+
+       def __str__(self):
+         return self.name
+
+#......post
+
+class Post(models.Model):
+        post_id = models.AutoField(primary_key=True)
+        title = models.CharField(max_length=100)
+        content = models.TextField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(null=False)
+        # user_id = models.IntegerField()
+        # category_id = models.IntegerField()
+        user = models.ForeignKey(User,on_delete=models.CASCADE)
+        category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+        class Meta:
+           db_table = 'post'
+
+        def __str__(self):
+           return self.title
+
+
+
+
+     
+    
--- blogpost/blog/views.py
+++ blogpost/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import *
+from rest_framework import viewsets
+from .serializers import *
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
